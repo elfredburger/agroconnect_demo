d17bef981dacf207849b28b560038eaeed273e77
elfredburger <elfredfredburger@gmail.com>
2024-06-11 18:06:30 +0200

    auth

---
+++ src/resources/auth/auth.controller.ts
@@ -0,0 +1,85 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import Controller from '../../utils/interfaces/controller.interface';
+import HttpException from '../../utils/exceptions/http.exception';
+import AuthService from '../../resources/auth/auth.service';
+import authenticated from '../../middleware/authenticated.middleware';
+class AuthController implements Controller {
+    public path = '/auth';
+    public router = Router();
+    private AuthService = new AuthService();
+
+    constructor() {
+        this.initialiseRoutes();
+    }
+    private initialiseRoutes(): void {
+        this.router.post(
+            `${this.path}/login`,
+
+            this.login,
+        );
+        this.router.post(`${this.path}/logout`, authenticated, this.logout);
+        this.router.post(`${this.path}/refresh`, authenticated, this.refresh);
+    }
+
+    private logout = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<Response | void> => {
+        try {
+            this.AuthService.logout(
+                req.headers.authorization?.replace('Bearer ', '') as string,
+            );
+            res.status(200).json('logged out');
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+
+    private refresh = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<Response | void> => {
+        const refreshToken = req.headers.authorization?.replace(
+            'Bearer ',
+            '',
+        ) as string;
+        try {
+            const token = await this.AuthService.refresh(refreshToken);
+            res.status(200).json({ token });
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+
+    private get = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<Response | void> => {
+        try {
+            await authenticated(req, res, next);
+
+            res.status(200).json('Secret data');
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+
+    private login = async (
+        req: Request,
+        res: Response,
+        next: NextFunction,
+    ): Promise<Response | void> => {
+        try {
+            const { email, password } = req.body;
+            const user = await this.AuthService.login(email, password);
+            res.status(200).json({ user });
+        } catch (error: any) {
+            next(new HttpException(400, error.message));
+        }
+    };
+}
+
+export default AuthController;
---
+++ src/resources/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import User from '@/utils/interfaces/user.interface';
+import token from '../../utils/token';
+import HttpException from '../../utils/exceptions/http.exception';
+import bcrypt from 'bcryptjs';
+import Token from '../../utils/interfaces/token.interface';
+import UserService from '../users/users.service';
+class AuthService {
+    private user = new UserService();
+
+    public async refresh(tokenInfo: string): Promise<String | Error> {
+        const user = await this.user.getUser({ token: tokenInfo });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        const newToken = await token.createToken(user, '7d');
+        await this.user.updateToken({ token: tokenInfo }, newToken);
+
+        return newToken;
+    }
+    public async login(email: string, password: string): Promise<User | Error> {
+        const userInfo = await this.user.getUser({ email: email });
+        if (!userInfo) {
+            throw new Error('User not found');
+        }
+        if (await bcrypt.compare(password, userInfo.password)) {
+            await this.user.updateToken(
+                { email: email },
+                token.createToken(userInfo, '7d'),
+            );
+            const updatedUser = (await this.user.getUser({
+                email: email,
+            })) as User;
+            return updatedUser;
+        } else {
+            throw new Error('Wrong password');
+        }
+    }
+    public async logout(token: string): Promise<String | Error> {
+        const user = await this.user.getUser({ token: token });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        await this.user.updateToken({ token: token }, '');
+        return 'User logged out';
+    }
+    public async verify(tokenInfo: string): Promise<Token | Error> {
+        const verify = await token.verifyToken(tokenInfo);
+        return verify;
+    }
+}
+export default AuthService;
